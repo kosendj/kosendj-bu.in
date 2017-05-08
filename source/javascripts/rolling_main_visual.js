455b134b7613e8b49174da819b5372d49c672674
asonas <hzw1258@gmail.com>
2017-05-08 23:16:24 +0900

    Separated main visual

--- source/javascripts/rolling_main_visual.js
+++ source/javascripts/rolling_main_visual.js
@@ -1,6 +1,10 @@
 $(function(){
   var index = Math.floor(Math.random()*6);
-  var url = 'images/pc/mv' + index + '.jpg';
+  var directory = "pc"
+  if(window.navigator.userAgent.toLowerCase().match(/(iphone|android)/i)) {
+    directory = "sp"
+  }
+  var url = 'images/' + directory + '/mv' + index + '.jpg';
   console.log(url)
   document.querySelector(".hero").style.cssText = "background-image: url("+ url +")";
 })
