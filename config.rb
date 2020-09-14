e231cb13019b3c4bb67ac0fad6bc2a95a94e08af
Yusuke Nakamura <yusuke1994525@gmail.com>
2020-09-14 17:44:44 +0900

    Cleanup gems and config

--- config.rb
+++ config.rb
@@ -1,11 +1,5 @@
 activate :livereload
 
-activate :deploy do |deploy|
-  deploy.build_before = true
-  deploy.deploy_method = :git
-  deploy.branch = 'gh-pages'
-end
-
 activate :external_pipeline,
   name: :sass,
   command: build? ? 'yarn run build' : 'yarn run watch',
