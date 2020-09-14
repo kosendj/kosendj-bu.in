ceb18168cc086cbeaf327dc50614592226b14660
Yusuke Nakamura <yusuke1994525@gmail.com>
2020-09-14 17:37:51 +0900

    Use external_pipeline

--- config.rb
+++ config.rb
@@ -1,20 +1,13 @@
 activate :livereload
 
-activate :sprockets do |c|
-  c.imported_asset_path = "assets"
-end
-
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-end
-
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
   deploy.branch = 'gh-pages'
 end
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
+activate :external_pipeline,
+  name: :sass,
+  command: build? ? 'yarn run build' : 'yarn run watch',
+  source: ".tmp",
+  latency: 1
