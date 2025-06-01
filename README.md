57fe38f97484dcddbad7f3d830c23b1495ad4285
HolyGrail <holygrail81@gmail.com>
2025-06-01 22:07:01 +0900

    replace yarn to pnpm

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 $ git clone git@github.com:kosendj/kosendj-bu.in.git
 $ cd kosendj-bu.in
 $ bundle install #require ruby
+$ pnpm install
 $ bundle exec middleman server
 ```
 
--- config.rb
+++ config.rb
@@ -2,6 +2,6 @@ activate :livereload, js_host: ENV['JS_HOST']
 
 activate :external_pipeline,
   name: :sass,
-  command: build? ? 'yarn run build' : 'yarn run watch',
+  command: build? ? 'pnpm run build' : 'pnpm run watch',
   source: ".tmp",
   latency: 1
