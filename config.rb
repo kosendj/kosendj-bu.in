3a15e4a7a493f9bd0b0fda7e6242169866d5d9b9
HolyGrail <holygrail81@gmail.com>
2025-06-04 08:48:59 +0900

    fix build error

--- config.rb
+++ config.rb
@@ -5,3 +5,7 @@ activate :external_pipeline,
   command: build? ? 'pnpm run build' : 'pnpm run watch',
   source: ".tmp",
   latency: 1
+
+# Ignore the source scss files since they're handled by external pipeline
+ignore 'stylesheets/*.scss'
+ignore 'stylesheets/_*.scss'
