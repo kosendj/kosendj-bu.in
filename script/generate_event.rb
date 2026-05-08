b0b90da4063becb1e83909b2d02a5d5c864ca8e9
Yusuke Nakamura <yusuke1994525@gmail.com>
2026-05-08 18:52:13 +0900

    Update timetable

--- script/generate_event.rb
+++ script/generate_event.rb
@@ -1,16 +1,18 @@
 require 'yaml'
 
 DATE = '2026.06.13 (Sat.) 14:00-20:00'
-TWIPLA_URL = 'https://twipla.jp/events/669873'
+TWIPLA_URL = 'https://twipla.jp/events/727590'
 
 INPUT = <<~TEXT
-  名前\tジャンル\t所要時間\t開始時間\t終了時刻
-  Takayuki Tominaga\tたのしい\t1:00:00\t14:00\t15:00
-  うなすけ\tProgressive House\t1:00:00\t15:00\t16:00
-  Rooq\t速いハウス\t1:00:00\t16:00\t17:00
-  furaji\t5月で現在の会社が無くなりまstep\t1:00:00\t17:00\t18:00
-  sylph01/G4きゅーぶ\tRubyIlluminationsの残り物(170+)\t1:00:00\t18:00\t19:00
-  HolyGrail\tRubyIlluminations 後夜祭\t1:00:00\t19:00\t20:00
+  名前	ジャンル	所要時間	開始時間	終了時刻
+  あそなす	CDJはやり方忘れてしまったので楽器でいい感じに...	0:45:00	14:00	14:45
+  うなすけ	Progressive House軸	0:45:00	14:45	15:30
+  Takayuki Tominaga	たのしい	0:45:00	15:30	16:15
+  Rooq	速いハウス	0:45:00	16:15	17:00
+  furaji	5月で現在の会社が無くなりまstep	0:45:00	17:00	17:45
+  ちゃんにく	グルミク収録曲	0:45:00	17:45	18:30
+  sylph01/G4きゅーぶ	RubyIlluminationsの残り物(170+)	0:45:00	18:30	19:15
+  HolyGrail	RubyIlluminations 後夜祭	0:45:00	19:15	20:00
 TEXT
 
 lines = INPUT.lines.map(&:chomp).reject(&:empty?)
